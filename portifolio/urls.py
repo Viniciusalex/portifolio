1b2ca993a8120c2ac34f2fc69671bd5c3130e933
Vinicius Alexandre <viniciusalexfernandes@gmail.com>
2023-05-29 15:44:22 -0300

    commit

--- portifolio/urls.py
+++ portifolio/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from portifolio.views import index
+from portifolio.views import index, sobreMim, apresentacao, conteudo
 
 
 urlpatterns = [
+    path('apresentacao', apresentacao, name='apresentacao'),
+    path('sobreMim', sobreMim, name='sobreMim'),
     path('', index, name='index'),
+    path('conteudo', conteudo, name='conteudo'),
 ]
--- portifolio/views.py
+++ portifolio/views.py
@@ -3,5 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def apresentacao(request):
+    return render(request, 'apresentacao.html')
+
+
+def sobreMim(request):
+    context = {
+        'pagina_atual': 'sobreMim'
+    }
+
+    return render(request, 'sobre_mim.html', context)
+
+
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'pagina_atual': 'index'
+    }
+    return render(request, 'index.html', context)
+
+
+def conteudo(request):
+    return render(request, 'conteudo.html')
